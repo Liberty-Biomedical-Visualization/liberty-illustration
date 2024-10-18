6c9b1d51685607e15729fbd730899a2dc6b029da
Nathaniel J. Liberty <nathaniel.liberty@gmail.com>
2024-10-17 23:17:54 -0400

    Add CarouselButton component

---
+++ src/components/Carousel/Button/index.spec.tsx
@@ -0,0 +1,64 @@
+import { describe, expect, jest, it } from "@jest/globals";
+import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import createTestRender from "@/utils/createTestRender";
+
+import CarouselButton from ".";
+
+describe("CarouselButton", () => {
+  it('should display a button with text "Next image" when type is next', () => {
+    renderCarouselButton({ type: "next" });
+    const result = screen.getByRole("button", { name: "Next image" });
+    expect(result).toBeVisible();
+  });
+
+  it('should display a button with text "Previous image" when type is previous', () => {
+    renderCarouselButton({ type: "previous" });
+    const result = screen.getByRole("button", { name: "Previous image" });
+    expect(result).toBeVisible();
+  });
+
+  it("should call onClick when clicked", async () => {
+    const onClick = jest.fn(() => {});
+    renderCarouselButton({ onClick, type: "next" });
+
+    const user = userEvent.setup();
+    const button = screen.getByRole("button", { name: "Next image" });
+    await user.click(button);
+
+    expect(onClick).toHaveBeenCalled();
+  });
+
+  it("should not call onClick when disabled", async () => {
+    const onClick = jest.fn(() => {});
+    renderCarouselButton({ disabled: true, onClick, type: "next" });
+
+    const user = userEvent.setup();
+    const button = screen.getByRole("button", { name: "Next image" });
+    await user.click(button);
+
+    expect(onClick).not.toHaveBeenCalled();
+  });
+
+  it("should pass className to the outermost element", () => {
+    const className = "foo";
+    const result = renderCarouselButton({ className });
+    expect(result.container.firstChild).toHaveClass(className);
+  });
+
+  it("should match the next button snapshot", () => {
+    const result = renderCarouselButton({ type: "next" });
+    expect(result.container).toMatchSnapshot();
+  });
+
+  it("should match the previous button snapshot", () => {
+    const result = renderCarouselButton({ type: "previous" });
+    expect(result.container).toMatchSnapshot();
+  });
+});
+
+const renderCarouselButton = createTestRender(CarouselButton, {
+  onClick: () => {},
+  type: "next",
+});
