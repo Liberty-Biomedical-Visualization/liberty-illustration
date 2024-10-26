07b9efb72bf4e2a863e2d00822fa94b227ec0c36
Nathaniel J. Liberty <nathaniel.liberty@gmail.com>
2024-10-26 08:09:00 -0400

    Add Brand component

---
+++ src/components/Header/Brand/index.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+
+import type { ImageData } from "@/lib/content";
+
+export default function Brand(props: BrandProps) {
+  const { className, logoData } = props;
+  const { description, height, src, title, width } = logoData;
+
+  return (
+    <div className={className}>
+      <Image
+        alt={description ?? ""}
+        className="max-w-3/4 mx-auto"
+        height={height}
+        src={src}
+        title={title}
+        priority
+        width={width}
+      />
+      <h1 className="font-light sm:text-3xl text-2xl text-center text-nowrap">
+        Abigail Richbourg Liberty, MS, CMI <br />
+        <span className="sm:text-lg text-sm">
+          Biomedical illustration • Animation • Graphic design
+        </span>
+      </h1>
+    </div>
+  );
+}
+
+export interface BrandProps {
+  className?: string;
+  logoData: ImageData;
+}
