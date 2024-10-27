862fd2af5982647703c5717c07e81692abc03499
Nathaniel J. Liberty <nathaniel.liberty@gmail.com>
2024-10-27 10:24:13 -0400

    Use configured homepage carousel images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import Carousel from "@/components/Carousel";
+import * as content from "@/lib/content";
 
-export default function Home() {
+export default async function Home() {
+  const configuration = await content.getJsonByTitle("Home Configuration");
+  const { carouselImageGalleryId } = configuration;
+  const imageGallery = await content.getImageGallery(carouselImageGalleryId);
+
   return (
     <Carousel
       className="h-64 lg:h-192 mx-4 sm:h-128 xs:h-72"
-      images={[]}
+      images={imageGallery.images}
       slideDuration={5_000}
       transitionDuration={1_000}
     />
