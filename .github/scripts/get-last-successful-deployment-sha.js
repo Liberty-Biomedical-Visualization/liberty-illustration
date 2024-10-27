75bbc71df75bca3341578b702f3e6b7dc007c9ba
Nathaniel J. Liberty <nathaniel.liberty@gmail.com>
2024-10-27 19:33:22 -0400

    Add get-last-successful-deployment-sha script

---
+++ .github/scripts/get-last-successful-deployment-sha.js
@@ -0,0 +1,17 @@
+module.exports = async ({ github, context }) => {
+  const deployments = await github.repos.listDeployments({
+    environment: process.env.ENVIRONMENT,
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+  });
+
+  const lastSuccessfulDeployment = deployments.data
+    .sort((a, b) => new Date(b.created_at) - new Date(a.created_at))
+    .find((deployment) => deployment.state === "success");
+
+  if (lastSuccessfulDeployment) {
+    return lastSuccessfulDeployment.sha;
+  }
+
+  throw new Error("successful deployment not found");
+};
