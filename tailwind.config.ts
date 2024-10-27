38cfd0889845f3373e932357bbed1db5f898e496
Nathaniel J. Liberty <nathaniel.liberty@gmail.com>
2024-10-27 10:17:31 -0400

    Add xs screen breakpoint

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -30,6 +30,9 @@ const config: Config = {
         "1/2": "50%",
         "3/4": "75%",
       },
+      screen: {
+        xs: "480px",
+      },
       spacing: {
         "260": "65rem",
       },
