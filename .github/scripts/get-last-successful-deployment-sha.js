4da4dff211ac297b6e20a7f4273e9f0d66b12058
Nathaniel J. Liberty <nathaniel.liberty@gmail.com>
2024-10-28 08:22:06 -0400

    Refine Deployment and Publish Workflow Behavior (#16)
    - Query deployment statuses
    - Give deployment priority over publish

--- .github/scripts/get-last-successful-deployment-sha.js
+++ .github/scripts/get-last-successful-deployment-sha.js
@@ -1,16 +1,31 @@
 module.exports = async ({ github, context }) => {
+  const { owner, repo } = context.repo;
+
   const deployments = await github.rest.repos.listDeployments({
     environment: process.env.ENVIRONMENT,
-    owner: context.repo.owner,
-    repo: context.repo.repo,
+    owner,
+    repo,
   });
 
-  const lastSuccessfulDeployment = deployments.data
-    .sort((a, b) => new Date(b.created_at) - new Date(a.created_at))
-    .find((deployment) => deployment.state === "success");
+  const sortedDeployments = deployments.data.sort(
+    (a, b) =>
+      new Date(b.created_at).getTime() - new Date(a.created_at).getTime(),
+  );
 
-  if (lastSuccessfulDeployment) {
-    return lastSuccessfulDeployment.sha;
+  for (const deployment of sortedDeployments) {
+    const statuses = await github.rest.repos.listDeploymentStatuses({
+      deployment_id: deployment.id,
+      owner,
+      repo,
+    });
+
+    const deploymentSucceeded = statuses.data.some(
+      (status) => status.state === "success",
+    );
+
+    if (deploymentSucceeded) {
+      return deployment.sha;
+    }
   }
 
   throw new Error("successful deployment not found");
