3dea7213a0c3603993e563b52c073f6894050b00
Nathaniel J. Liberty <nathaniel.liberty@gmail.com>
2025-01-20 12:16:09 -0500

    Remove support for local images from loadContentfulImage

--- loadContentfulImage.ts
+++ loadContentfulImage.ts
@@ -2,12 +2,6 @@ import type { ImageLoaderProps } from "next/image";
 
 export default function loadContentfulImage(props: ImageLoaderProps) {
   const { quality, src, width } = props;
-
-  // @TODO: Remove. All images should come from Contentful.
-  if (src.startsWith("/_next/")) {
-    return src;
-  }
-
   const url = new URL(src);
   url.searchParams.set("fm", "webp");
   url.searchParams.set("q", (quality ?? 75).toString());
