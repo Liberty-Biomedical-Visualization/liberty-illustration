e8e3d33981a56adf4a004a2971805d5e6698c49c
Nathaniel J. Liberty <nathaniel.liberty@gmail.com>
2025-01-20 11:29:59 -0500

    Add sendContactEmail API function

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,2 +1,3 @@
 export { default as makeRoute, type RouteConfiguration } from "./makeRoute";
 export { default as parseJsonBody } from "./parseJsonBody";
+export { default as sendContactEmail } from "./sendContactEmail";
